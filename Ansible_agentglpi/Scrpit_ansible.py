acd0ade6e9dd5ff74d8be5542fd88461075902ac
Lionel BELLEROSE <lionel.bellerose@outlook.com>
2021-08-20 18:32:18 +0200

    Structure du script et modification fonction install_dependencies et send_info.

--- Ansible_agentglpi/Scrpit_ansible.py
+++ Ansible_agentglpi/Scrpit_ansible.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python 
 # -*- coding: utf-8 -*-
 
-#0. Importation de la classe Ansible module 
+#0. Importation de la classe Ansible module :
 from ansible.module_utils.basic import AnsibleModule
 
-#1. Importation du module Python OS pour interagir avec DEBIAN
+#1. Importation du module Python OS pour interagir avec DEBIAN :
 import os
 
-#2. Définition fonction install_dependencies
+#2. Définition fonction install_dependencies :
 def install_dependencies():
-	os.system("apt install -y dmidecode hwdata ucf hdparm")
-	os.system("apt -y install perl libuniversal-require-perl libwww-perl libparse-edid-perl")
-	os.system("apt -y install libproc-daemon-perl libfile-which-perl libhttp-daemon-perl")
-	os.system("apt -y install libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl")
-	os.system("apt -y install libdigest-sha-perl libsocket-getaddrinfo-perl libtext-template-perl")
-	os.system("apt -y install libxml-xpath-perl libyaml-tiny-perl")
+	os.system("apt install -y dmidecode hwdata ucf hdparm perl libuniversal-require-perl libwww-perl libparse-edid-perl libproc-daemon-perl libfile-which-perl libhttp-daemon-perl libxml-treepp-perl libyaml-perl libnet-cups-perl libnet-ip-perl libdigest-sha-perl libsocket-getaddrinfo-perl libtext-template-perl libxml-xpath-perl libyaml-tiny-perl")
 
-#3. Définition fonction download_agentFI
+#3. Définition fonction download_agentFI :
 def download_agentFI():
 	os.system("wget https://github.com/fusioninventory/fusioninventory-agent/releases/download/2.5.2/fusioninventory-agent_2.5.2-1_all.deb")
 
-#4. Définition fonction dpkg_agentFI
+#4. Définition fonction dpkg_agentFI :
 def dpkg_agentFI():
 	os.system("dpkg -i fusioninventory-agent_2.5.2-1_all.deb")
 
 #5. Définition fonction config_agent
-def config_agent(server_url, delay_time):
+def config_agent():
 	pass
 
-#6. Definition fonction envoie information du systeme
+#6. Definition fonction envoie information du systeme:
 def send_info():
+
+	# Redémarrage du service Fusioninventory_agent :
+	os.system("systemctl restart fusioninventory-agent.service")
+
+	# Envoi des informations système au serveur cible :
 	os.system("pkill -USR1 -f -P 1 fusioninventory-agent")
+
 #7. Définition du Module Ansible
 def main():
-	module_args = dict(
-		server_url=dict(type='str', required=True)
-		delay_time=dict(type='int', required=False, default=3600)
-	)
-
-	module = AnsibleModule(argument_spec=module_args)
+	
 	
 	install_dependencies()
 	download_agentFI()
